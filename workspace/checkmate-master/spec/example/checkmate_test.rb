df070233f8f5962efeb2e329ffacfae0510f645b
Tristan Beasley <twbeasley13@gmail.com>
2018-09-30 16:56:43 -0400

    no examples found

---
+++ workspace/checkmate-master/spec/example/checkmate_test.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+
+RSpec.describe 'Hello World' do
+  it 'should be true' do
+    expect(true).to eq true
+  end
+end
--- workspace/checkmate-master/spec/models/piece_spec.rb
+++
@@ -1,7 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe Piece, type: :model do
-    test "the truth" do
-  assert "true"
-    end
-end
